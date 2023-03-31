ffcd22ffbef7151d0da85c8caa312ab6ce593f71
Jônatas Gomes <jongaglima@gmail.com>
2023-03-30 23:33:34 -0300

    Feat and Style: stylizes the interface and also adds the video and its functionalities

---
+++ src/pages/video.js
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from "react";
+import "./video.css";
+
+function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handleStart}
+        loop
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
