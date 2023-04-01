7526acea75a6a4adf53c88bdac5c712bbf7a1611
Jônatas Gomes <jongaglima@gmail.com>
2023-04-01 17:54:18 -0300

    Feat and Style: Adds the sidebar and styles it

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Test Name"
+          description="Test Description"
+          music="Test Music Name"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video
+          likes={444}
+          messages={555}
+          shares={666}
+          name="Test Name 2"
+          description="Test Description 2"
+          music="Test Music Name 2"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
       </div>
     </div>
   );
--- src/pages/components/footer/VideoFooter.js
+++ src/pages/components/footer/VideoFooter.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./videoFooter.css";
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
 
-function VideoFooter() {
+function VideoFooter({ name, description, music }) {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
-        <h3>@Jônatas Ebac</h3>
-        <p>Descrição do Vídeo</p>
+        <h3>@{name}</h3>
+        <p>{description}</p>
         <div className="videoFooter__music">
           <MusicNoteIcon className="videoFooter__icon" />
           <div className="videoFooterMusic__text">
-            <p>Titulo da musica</p>
+            <p>{music}</p>
           </div>
         </div>
       </div>
